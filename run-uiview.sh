52955e2d58f93fb7844c27d9d07ee4cf08f7c355
Samuel Coles <me@smurf.codes>
2021-05-23 00:58:55 -0500

    Add run-uiview.sh

---
+++ run-uiview.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker run -it --rm --gpus all \
+	-e DISPLAY=$DISPLAY \
+	-e PASSIVE=0 \
+	-e NOSENSOR=1 \
+	-e USE_WEBCAM=1 \
+  -e ROADCAM_ID=0 \
+  -e DRIVERCAM_ID=2 \
+	-e QT_DEBUG_PLUGINS=1 \
+        -e QT_X11_NO_MITSHM=1 \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+    --device=/dev/video0:/dev/video0 \
+    --device=/dev/video2:/dev/video2 \
+    --device=/dev/dri:/dev/dri \
+	openpilot:latest \
+  /root/openpilot/selfdrive/debug/uiview.py
