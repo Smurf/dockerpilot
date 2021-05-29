bd9bab353a3724728ce237214b595c7cb41faf06
SamuelColes <scoles@localhost.localdomain>
2021-05-28 22:43:28 -0500

    Update run .sh files

--- run-uiview.sh
+++ run-uiview.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-docker run -it --rm --gpus all \
+xhost +local:docker
+docker run -it --gpus all \
 	-e DISPLAY=$DISPLAY \
 	-e PASSIVE=0 \
 	-e NOSENSOR=1 \
 	-e USE_WEBCAM=1 \
-  -e ROADCAM_ID=0 \
-  -e DRIVERCAM_ID=2 \
 	-e QT_DEBUG_PLUGINS=1 \
-        -e QT_X11_NO_MITSHM=1 \
+    -e QT_X11_NO_MITSHM=1 \
+    --security-opt seccomp:unconfined \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-    --device=/dev/video0:/dev/video0 \
-    --device=/dev/video2:/dev/video2 \
+    --device=/dev/video2:/dev/video0 \
+    --device=/dev/video4:/dev/video1 \
     --device=/dev/dri:/dev/dri \
-	openpilot:latest \
-  /root/openpilot/selfdrive/debug/uiview.py
+	192.168.0.110:5000/openpilot:latest \
+    bash
+    #/root/openpilot/selfdrive/debug/uiview.py
--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ docker run -it --rm --gpus all \
 	-e NOSENSOR=1 \
 	-e USE_WEBCAM=1 \
 	-e QT_DEBUG_PLUGINS=1 \
-        -e QT_X11_NO_MITSHM=1 \
+    -e QT_X11_NO_MITSHM=1 \
+    -e USE_WEBCAM=1 \
+    --security-opt seccomp:unconfined \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-    --device=/dev/video0:/dev/video0 \
-    --device=/dev/video2:/dev/video2 \
+    --device=/dev/video4:/dev/video0 \
+    --device=/dev/video0:/dev/video1 \
     --device=/dev/dri:/dev/dri \
-	openpilot:latest \
+	192.168.0.110:5000/openpilot:latest \
 	/root/openpilot/selfdrive/manager/manager.py
