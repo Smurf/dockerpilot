de55b585316656f7868099fd4c72c99dcca47abd
smurf_codes <me@smurf.codes>
2021-05-28 23:14:46 -0500

    Update dockerfiles, move back to apt for capnp

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,4 @@
 docker build -f Dockerfile.base . -t openpilot:base
 docker build -f Dockerfile.stage2 . -t openpilot:stage2
+docker build -f Dockerfile.stage3 . -t openpilot:stage3
 docker build -f Dockerfile.build . -t openpilot:latest
