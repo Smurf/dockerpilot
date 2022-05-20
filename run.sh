6c2724fc75a5f11e3ba628f205de27bdce456972
Sam Coles <sammuel.coles@gmail.com>
2022-05-20 00:03:08 -0500

    Cleanup

--- run.sh
+++ run.sh
@@ -9,8 +9,6 @@ docker run -it --rm --gpus all \
 	-e QT_DEBUG_PLUGINS=1 \
   -e QT_X11_NO_MITSHM=1 \
   -e USE_WEBCAM=1 \
-  -e __NV_PRIME_RENDER_OFFLOAD=1 \
-  -e __GLX_VENDOR_LIBRARY_NAME=nvidia \
   --security-opt seccomp:unconfined \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
   --device=/dev/video2:/dev/video0 \
